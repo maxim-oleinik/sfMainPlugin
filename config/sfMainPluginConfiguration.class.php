4a65ec19a5c3981e2dbecc82cdf2aa750333350d
Maxim Oleinik <maxim.oleinik@gmail.com>
2011-03-18 17:11:49 +0300

    [Config] Add plugins config - register sfMainCommonHelper as standart helper

---
+++ config/sfMainPluginConfiguration.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin config
+ */
+class sfMainPluginConfiguration extends sfPluginConfiguration
+{
+    /**
+     * Init
+     */
+    public function initialize()
+    {
+        // Merge helpers
+        $helpers = array_merge(sfConfig::get('sf_standard_helpers', array()), array('sfMainCommon'));
+        sfConfig::set('sf_standard_helpers', $helpers);
+    }
+
+}
